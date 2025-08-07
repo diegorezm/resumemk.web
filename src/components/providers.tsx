8bcc9630bb6aa5cb16176294f92207d78bf28c82
diegorezm <dmourarezende@gmail.com>
2025-08-07 11:45:35 -0300

    toast.error

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -5,10 +5,10 @@ import { ClerkProvider } from "@clerk/tanstack-react-start";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ConvexQueryClient } from "@convex-dev/react-query";
 import {
-  MutationCache,
-  QueryCache,
-  QueryClient,
-  QueryClientProvider,
+	MutationCache,
+	QueryCache,
+	QueryClient,
+	QueryClientProvider,
 } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { ConvexError } from "convex/values";
@@ -17,73 +17,73 @@ import i18n from "@/lib/i18n";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Add your Clerk Publishable Key to the .env.local file");
+	throw new Error("Add your Clerk Publishable Key to the .env.local file");
 }
 
 const CONVEX_URL = import.meta.env.VITE_CONVEX_URL;
 
 if (!CONVEX_URL) {
-  throw new Error("missing envar CONVEX_URL");
+	throw new Error("missing envar CONVEX_URL");
 }
 
 export const convexQueryClient = new ConvexQueryClient(CONVEX_URL);
 
 const queryClient: QueryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      queryKeyHashFn: convexQueryClient.hashFn(),
-      queryFn: convexQueryClient.queryFn(),
-    },
-  },
-  queryCache: new QueryCache({
-    onError: (e) => {
-      if (e instanceof ConvexError) {
-        toast(e.data.message, {
-          position: "bottom-right",
-        });
-      } else {
-        toast("Something went wrong.", {
-          position: "bottom-right",
-        });
-      }
-    },
-  }),
-  mutationCache: new MutationCache({
-    onError: (e) => {
-      if (e instanceof ConvexError) {
-        toast(e.data.message, {
-          position: "bottom-right",
-        });
-      } else {
-        toast("Something went wrong.", {
-          position: "bottom-right",
-        });
-      }
-    },
-  }),
+	defaultOptions: {
+		queries: {
+			queryKeyHashFn: convexQueryClient.hashFn(),
+			queryFn: convexQueryClient.queryFn(),
+		},
+	},
+	queryCache: new QueryCache({
+		onError: (e) => {
+			if (e instanceof ConvexError) {
+				toast(e.data.message, {
+					position: "bottom-right",
+				});
+			} else {
+				toast.error("Something went wrong.", {
+					position: "bottom-right",
+				});
+			}
+		},
+	}),
+	mutationCache: new MutationCache({
+		onError: (e) => {
+			if (e instanceof ConvexError) {
+				toast(e.data.message, {
+					position: "bottom-right",
+				});
+			} else {
+				toast.error("Something went wrong.", {
+					position: "bottom-right",
+				});
+			}
+		},
+	}),
 });
 
 convexQueryClient.connect(queryClient);
 
 export function getContext() {
-  return {
-    queryClient: convexQueryClient.queryClient,
-  };
+	return {
+		queryClient: convexQueryClient.queryClient,
+	};
 }
 
 export function Providers({ children }: { children: ReactNode }) {
-  return (
-    <I18nextProvider i18n={i18n}>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-        <ConvexProviderWithClerk
-          client={convexQueryClient.convexClient}
-          useAuth={useAuth}
-        >
-          <QueryClientProvider client={convexQueryClient.queryClient}>
-            {children}
-          </QueryClientProvider>
-        </ConvexProviderWithClerk>
-      </ClerkProvider>
-    </I18nextProvider>
-  );
+	return (
+		<I18nextProvider i18n={i18n}>
+			<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+				<ConvexProviderWithClerk
+					client={convexQueryClient.convexClient}
+					useAuth={useAuth}
+				>
+					<QueryClientProvider client={convexQueryClient.queryClient}>
+						{children}
+					</QueryClientProvider>
+				</ConvexProviderWithClerk>
+			</ClerkProvider>
+		</I18nextProvider>
+	);
 }
