c95003417dffc2bea743050cf5890d100e8a2417
diegorezm <dmourarezende@gmail.com>
2025-08-06 21:43:01 -0300

    better css reset

--- src/modules/resume/ui/components/markdown-preview.tsx
+++ src/modules/resume/ui/components/markdown-preview.tsx
@@ -3,59 +3,89 @@ import { marked } from "marked";
 import { type RefObject, useEffect } from "react";
 
 const cssReset = `
-  html, body {
-    all: unset;
-    margin: 0;
-    padding: 0;
-    font-family: sans-serif;
-    all: unset;
-    box-sizing: border-box;
-  }
-  hr {
-    margin: 0.5rem 0;
-    border: none;
-    border-top: 1px solid #ccc;
-  }
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+
+body, h1, h2, h3, h4, h5, h6, p, ol, ul {
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+
+ol, ul {
+  list-style: none;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+a::before, a::after {
+  display: none !important;
+  content: none !important;
+}
+
+a {
+  text-decoration: none;
+  color: blue;
+}
+
+ul {
+  list-style-type: disc;
+  margin-top: 0.5rem;
+  margin-left: 1rem;
+}
+
+li {
+  margin-bottom: 0.5rem;
+}
+
+em {
+  font-style: italic;
+}
+
+strong {
+  font-weight: bold;
+}
 `;
 
 interface Props {
-  title: string;
-  markdown: string;
-  css: string;
-  iframeRef: RefObject<HTMLIFrameElement | null>;
+	title: string;
+	markdown: string;
+	css: string;
+	iframeRef: RefObject<HTMLIFrameElement | null>;
 }
 
-const renderer = new marked.Renderer();
-
-renderer.link = ({ href, title, text }) => {
-  const titleAttr = title ? ` title="${title}"` : "";
-  return `<a href="${href}" target="_blank" rel="noopener noreferrer"${titleAttr}>${text}</a>`;
-};
-
 export function MarkdownPreview({ title, markdown, css, iframeRef }: Props) {
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    if (!iframe) return;
+	useEffect(() => {
+		const iframe = iframeRef.current;
+		if (!iframe) return;
 
-    const doc = iframe.contentDocument || iframe.contentWindow?.document;
-    if (!doc) return;
+		const doc = iframe.contentDocument || iframe.contentWindow?.document;
+		if (!doc) return;
 
-    const html = marked(markdown, {
-      async: false,
-      gfm: true,
-      renderer,
-    });
-    const sanitizedHTML = DOMPurify.sanitize(html, {
-      ADD_ATTR: ["target", "rel"],
-    });
+		const html = marked(markdown, {
+			async: false,
+			gfm: true,
+		});
 
-    const fullHtml = `
+		const sanitizedHTML = DOMPurify.sanitize(html, {
+			ADD_ATTR: ["target", "rel"],
+		});
+
+		const fullHtml = `
 			<!DOCTYPE html>
 			<html>
 			<head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js" integrity="sha512-GsLlZN/3F2ErC5ifS5QtgpiJtWd43JWSuIgh7mbzZ8zBps+dvLusV+eNQATqgA/HdeKFVgA5v3S/cIrLF7QnIg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
       <title>${title}</title>
       <style>${cssReset}</style>
 				<style>
@@ -63,46 +93,22 @@ export function MarkdownPreview({ title, markdown, css, iframeRef }: Props) {
 				</style>
 			</head>
 			<body>
-<main id="resume">
-
 				${sanitizedHTML}
-</main>
-        <button id="download-btn" style="display: none;" onclick="downloadPdf()">Download PDF</button>
-    <script>
-const resumeContainer = document.getElementById("resume")
-      window.downloadPdf = function () {
-        html2pdf().set({
-    filename: "${title}.pdf",
-    html2canvas: {
-      scale: 2,         // Increase resolution
-      useCORS: true,    // Allow external resources like fonts/images
-      allowTaint: false // Prevent CORS errors
-    },
-    jsPDF: {
-      unit: "px",       // Points (1/72 inch)
-      format: "a4",     // Standard A4 paper
-      orientation: "portrait",
-      hotfixes: ["px_scaling"] 
-    },
-        }).from(resumeContainer).save().catch(e => console.error(e));
-
-      };
-    </script>
 			</body>
 			</html>
 		`;
 
-    doc.open();
-    doc.writeln(fullHtml);
-    doc.close();
-  }, [markdown, css, title, iframeRef.current]);
+		doc.open();
+		doc.writeln(fullHtml);
+		doc.close();
+	}, [markdown, css, title, iframeRef.current]);
 
-  return (
-    <iframe
-      id="markdown-preview-frame"
-      ref={iframeRef}
-      title="Markdown Preview"
-      className="w-full h-full border-none rounded"
-    />
-  );
+	return (
+		<iframe
+			id="markdown-preview-frame"
+			ref={iframeRef}
+			title="Markdown Preview"
+			className="w-full h-full border-none rounded"
+		/>
+	);
 }
